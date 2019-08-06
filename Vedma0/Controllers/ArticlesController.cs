c9a7b9a6e56b5d702bfa33c4404f032dbab44671
Tony ermakov <demimos.ll@gmail.com>
2019-08-06 13:07:37 +0300

    qq

---
+++ Vedma0/Controllers/ArticlesController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Vedma0.Data;
+using Vedma0.Models.GameEntities;
+using Vedma0.Models.Helper;
+
+namespace Vedma0.Controllers
+{
+    [AccessRule(AccessLevel.Developer)]
+    public class ArticlesController : VedmaController
+    {
+
+        public ArticlesController(ApplicationDbContext context):base(context)
+        {
+        }
+
+        // GET: Articles
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Articles.Include(a => a.Game).Include(a => a.Publisher);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Articles/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var article = await _context.Articles
+                .AsNoTracking()
+                .Include(a => a.Publisher)
+                .FirstOrDefaultAsync(m => m.Id == id && m.GameId==GameId());
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            return View(article);
+        }
+
+        // GET: Articles/Create
+        public IActionResult Create(long? publisherId)
+        {
+            if (publisherId == null || !PublisherExists((long)publisherId))
+                return NotFound();
+            ViewData["PublisherId"] = publisherId;
+            return View();
+        }
+
+        // POST: Articles/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Image,Body,PublisherId, Name")] Article article)
+        {
+            if (ModelState.IsValid && article.PublisherId!=null && PublisherExists((long)article.PublisherId))
+            {
+                article.GameId = (Guid)GameId();
+                _context.Add(article);
+                //photo logic insert here
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["PublisherId"] = article.PublisherId;
+            return View(article);
+        }
+
+        // GET: Articles/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var article = await _context.Articles
+                .AsNoTracking()
+                .FirstOrDefaultAsync(a => a.Id==id );
+            if (article == null || !PublisherExists((long)article.PublisherId))
+            {
+                return NotFound();
+            }
+            return View(article);
+        }
+
+        // POST: Articles/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("Image,Body,DateTime,Name")] Article article)
+        {
+            if (id != article.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(article);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ArticleExists(article.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+          
+            return View(article);
+        }
+
+        // GET: Articles/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var article = await _context.Articles
+                .AsNoTracking()
+                .Include(a => a.Publisher)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            return View(article);
+        }
+
+        // POST: Articles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var article = await _context.Articles.FindAsync(id);
+            _context.Articles.Remove(article);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ArticleExists(long id)
+        {
+            return _context.Articles.Any(e => e.Id == id && e.GameId==GameId());
+        }
+        private bool PublisherExists(long id)
+        {
+            return _context.Publisher.Any(e => e.Id == id && e.GameId == GameId());
+        }
+    }
+}
--- Vedma0/Models/GameEntities/Article.cs
+++ Vedma0/Models/GameEntities/Article.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,12 @@ namespace Vedma0.Models.GameEntities
 {
     public class Article : GameEntity
     {
+
+        public Article() : base()
+        {
+            Images = new List<string>();
+        }
+
         /// <summary>
         /// No root, m_*** - image medium
         /// s_*** - image small
@@ -14,7 +22,16 @@ namespace Vedma0.Models.GameEntities
         public string Image { get; set; }
         public string Body { get; set; }
         public DateTime DateTime { get; set; }
-      
+        
+        public string _Images { get; set; }
+
+        [NotMapped]
+        public IList<string> Images
+        {
+            get => JsonConvert.DeserializeObject<IList<string>>(_Images);
+            set => _Images = JsonConvert.SerializeObject(value);
+        }
+
         public long? PublisherId { get; set; }
         public Publisher Publisher { get; set; }
     }
