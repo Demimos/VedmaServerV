02b5f959cd3f6f7d3ea850550332e95751ddbcd2
Tony ermakov <demimos.ll@gmail.com>
2019-08-06 16:18:43 +0300

    ss

---
+++ Vedma0/Migrations/20190806131310_eith.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vedma0.Migrations
+{
+    public partial class eith : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "_Images",
+                table: "GameEntities",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "_Images",
+                table: "GameEntities");
+        }
+    }
+}
